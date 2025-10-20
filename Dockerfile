f432221a9b12128efa39358848caec4c4fa59e2d
Humberto Manuel Cabrera Cervantes <humbertomanuel_cabrera@outlook.es>
2025-10-20 17:35:40 -0600

    Runners

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,26 @@
-FROM golang:1.21-alpine AS builder
+FROM golang:1.23-alpine AS builder
 
 WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY . .
+
+RUN go build -o server ./cmd/demo.go
+
+# ___________RUNTIME STAGE_____________
+
+FROM alpine:latest
+
+RUN apk add --no-cache ca-certificates
+
+WORKDIR /app
+
+COPY --from=builder /app/server /app/server
+
+EXPOSE 4430
+
+ENV SYNERGYNET_PORT=4430
+
+CMD ["/app/server"]
--- cmd/demo.go
+++ cmd/demo.go
@@ -1,7 +1,37 @@
 package main
 
-import SynergyNetServer "github.com/HManuelCC/SynergyNetServer/Server"
+import (
+	"os"
+	"strconv"
+
+	SynergyNetServer "github.com/HManuelCC/SynergyNetServer/Server"
+)
 
 func main() {
-	SynergyNetServer.NewSocketServer(443)
+
+	//recibir puerto desde consola o variable de entorno
+	var port int
+
+	args := os.Args
+	if len(args) > 1 {
+		//convertir a int
+		//si no es un numero, usar 443
+		var err error
+		port, err = strconv.Atoi(args[1])
+		if err != nil {
+			port = 443
+		}
+	} else {
+		//revisar variable de entorno
+		envPort := os.Getenv("SYNERGYNET_PORT")
+		if envPort != "" {
+			var err error
+			port, err = strconv.Atoi(envPort)
+			if err != nil {
+				port = 443
+			}
+		}
+	}
+
+	SynergyNetServer.NewSocketServer(port)
 }
